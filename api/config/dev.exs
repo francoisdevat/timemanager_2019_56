ae717fa8c8232e10063a17ee43655307919e97cf
Samorinho <46651876+Samorinho@users.noreply.github.com>
2020-10-26 09:17:22 +0100

    Update dev.exs

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :api, Gotham.Repo,
   username: "postgres",
-  password: "psotgres",
+  password: "postgres",
   database: "api_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
