1a3448427cc768b9ac2c098e192d21d17323b660
georges <georges.bitakwire@epitech.eu>
2020-11-04 13:54:31 +0100

    front-end backend db seems good work

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :api, Gotham.Repo,
   username: "postgres",
-  password: "azerty",
+  password: "postgres",
   database: "api_dev",
   hostname: "db",
   show_sensitive_data_on_connection_error: true,
